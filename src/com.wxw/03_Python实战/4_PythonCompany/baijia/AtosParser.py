bedabe1eb556a6c1af670e5c8eb0a5ed5d936f7d
weixiaowei <weixiaowei@baijia.com>
2021-05-14 14:13:20 +0800

    atosl

---
+++ src/com.wxw/03_Python实战/4_PythonCompany/baijia/AtosParser.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/14 12:27
+# @Author  : wxw
+# @Site    :
+# @File    : AtosParser.py
+
+def parse_atosl():
+    print("解析 atosl")
+
+
+if __name__ == '__main__':
+    parse_atosl()
---
+++ src/com.wxw/03_Python实战/4_PythonCompany/baijia/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/14 12:27
+# @Author  : wxw
+# @Site    : 
+# @File    : __init__.py.py
