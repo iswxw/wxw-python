c28406559721c03e878f80ba1a8de8de593333df
weixiaowei <weixiaowei@didichuxing.com>
2021-12-25 16:01:40 +0800

    update local and redis script

---
+++ src/com.wxw/study/py1_base/py12_os/case_os01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3  
+# -*- coding:utf-8 _*-
+""" 
+@author:xiaowei
+@file: case_os01.py 
+@time: 2021/12/25 
+"""
+
+import os
+
+# [1] 获取当前工作目录
+print(os.getcwd())  # /Users/didi/Documents/didi2021/python/wxw-python/src/com.wxw/study/py1_base/py12_os
+
+# [2] 获取当前目录的父目录
+print(os.pardir)  # 输出默认值 ..
--- src/com.wxw/study/py2_advance/py21_script/py211_command/py2111_csv_parse_image/base64_to_image_local.py
+++ src/com.wxw/study/py2_advance/py21_script/py211_command/py2111_csv_parse_image/base64_to_image_local.py
@@ -6,11 +6,16 @@
 @time: 2021/12/22
 """
 import base64
+import os
 import sys
 import redis
 import pandas as pd
 from optparse import OptionParser
 from optparse import IndentedHelpFormatter
+
+num_partitions = 2  # 数据分割粒度
+img_suffix = ".jpg"  # 图片后缀
+img_path = "/images/"  # 图片所在包名称
 
 
 # [1] 格式化包装help
@@ -30,7 +35,7 @@ parser = OptionParser(
 parser.add_option("-H", "--host", action="store", help="Redis主机地址IP", default="127.0.0.1")
 parser.add_option("-P", "--port", action="store", help="Redis端口", default="6379")
 parser.add_option('-A', "--password", action="store", help="Redis密码", default="321")
-parser.add_option('-I', "--in_file", action="store", help="指定待转换xlsx文件")
+parser.add_option('-I', "--in_file", action="store", help="指定待转换csv文件")
 parser.add_option('-O', "--out_file", action="store", help="指定输出xlsx文件")
 
 
@@ -42,42 +47,52 @@ def redis_client(params):
     return r
 
 
+# [4] 获取文件路径
+def get_file_path(redis_key):
+    path = os.getcwd() + img_path
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path + redis_key + img_suffix
+
+
 def base64_2_image_by_read_redis(rewards, params):
     # [1] 加载redis 配置
     r = redis_client(params)
-    print(params.host, params.port, params.password)
-
     # [2] 解析奖励信息
     result = pd.DataFrame()
+    i = 0
+    # [3] 定义xlsx文件
+    writer = pd.ExcelWriter(params.out_file)
+    # 写数据
     for row in rewards.itertuples():
-        # [3] redis 获取img key = img
+        # [4] redis 获取img key = img
         base64_str = r.get('img')
-        # [4] 转换后的图片链接 上传文件返回链接
+        # [5] 转换后的图片链接 上传文件返回链接
         imgdata = base64.b64decode(base64_str)
-
-        # [5] 图片路径
-
-        file = open('img.jpg', 'wb')
+        # [6] 图片路径
+        file_path = get_file_path(row.redis_key)
+        file = open(file_path, 'wb')
         file.write(imgdata)
         file.close()
         # 追加数据
-        img_url = "https://img0.baidu.com/it/u=2866200409,4132400541&fm=253&fmt=auto&app=120&f=JPEG?w=450&h=780"
         result = result.append(
             {
                 "用户ID": row.user_id,
                 "姓名": row.name,
                 "地址": row.addr,
                 "图片Key": row.redis_key,
-                "图片地址": img_url
+                "图片地址": file_path
             },
             ignore_index=True)
-
-    # [5] 写入输出csv文件
-    result.to_csv(params.out_file, index=False)
-
+        # [5] 写入输出xlsx文件
+        result.to_excel(writer, sheet_name='sheet_' + str(int(i / num_partitions)))
+        i = i + 1
+        if i % num_partitions == 0:
+            result = result.drop(index=result.index)
+    writer.save()
 
 # command
-#  - ./base64_to_image_redis.py -I in_file.xlsx -O test.xlsx
+#  - ./base64_to_image_redis.py -I in_file.csv -O test.xlsx
 if __name__ == '__main__':
     (options, args) = parser.parse_args(sys.argv[1:])
     if not (options.in_file and options.out_file):
